fb01ec8df868bc60b2c9dd55637ba5bc46a80674
Justin Lucas <200282171@student.georgianc.on.ca>
2020-07-20 20:42:04 -0400

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by Justin Lucas on 2020-07-20.
+//  Copyright © 2020 Justin Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
